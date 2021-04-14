1bc60007bdbbcf7afd35796f797c250732fce678
XiwayBanh <xiway.banh@gmail.com>
2021-04-14 09:45:53 +0800

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,4 @@ Rails.application.routes.draw do
     resources :tabs
   end
   get 'auth/request', to:'users#google_oauth2'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
