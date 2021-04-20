c82e68b371695e1efcdc2da78d3b137f53515821
Kevin Kurczseitz <kevinkurczseitz@icloud.com>
2021-04-20 12:16:39 +0800

    unsaved tabs show up now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
   skip_before_action :verify_authenticity_token
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :unsaved_tabs
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
@@ -10,4 +11,9 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
+
+  def unsaved_tabs
+    @unsaved_tabs = Tab.joins(:folder).where(:folders => {name:'Default'})
+  end
+
 end
