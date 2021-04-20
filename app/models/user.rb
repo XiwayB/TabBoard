e5e9f63412e55583cb464ed68383cada90b0f5b5
XiwayBanh <xiway.banh@gmail.com>
2021-04-20 13:22:09 +0800

    fetch user_email and sign-in user in the back-end

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,10 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     p auth
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    # where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    where(email: auth.info.email).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
       user.name = auth.info.name
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'folders#index'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  get 'users/login_from_ext', to: 'users#login_from_ext'
+  post 'users/login_from_ext', to: 'users#login_from_ext'
   # namespace :api, defaults: { format: :json } do
   #   namespace :v1 do
   #   end
