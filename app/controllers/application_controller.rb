b905787d1672f2dcf2c932de891fe62e96831348
Desmond Ip <ipkinghangdesmond@gmail.com>
2021-04-20 11:46:14 +0800

    I CAN SHAAARE AND view shareees

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
   skip_before_action :verify_authenticity_token
   before_action :configure_permitted_parameters, if: :devise_controller?
 
--- app/javascript/scripts/show_share.js
+++ app/javascript/scripts/show_share.js
@@ -1,10 +1,5 @@
-
-
-
 const sharePopup = () => {
   console.log("share popping up");
-  // document.addEventListener('DOMContentLoaded', function() {
-
     const showForm = document.getElementById("show-share-form");
     console.log({showForm})
     if (showForm) {
@@ -13,24 +8,16 @@ const sharePopup = () => {
         document.getElementById("new-share-form").removeAttribute("class");
       });
     }
-
-  // }, false);
-
 }
 
 const closeSharePopup = () => {
   console.log("closing share popup");
-  // document.addEventListener('DOMContentLoaded', function() {
-
-    const closeBtn = document.getElementById("close-share-form");
-    if (closeBtn) {
-      closeBtn.addEventListener('click', ()=>{
-        document.getElementById("new-share-form").setAttribute("class", "hidden");
-      });
-    }
-
-  // }, false);
-
+  const closeBtn = document.getElementById("close-share-form");
+  if (closeBtn) {
+    closeBtn.addEventListener('click', ()=>{
+      document.getElementById("new-share-form").setAttribute("class", "hidden");
+    });
+  }
 }
 
 
