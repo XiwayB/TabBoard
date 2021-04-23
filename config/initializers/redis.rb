8bce317f3bf4d991beb7dd92f455b0cf77d7645e
XiwayBanh <xiway.banh@gmail.com>
2021-04-23 11:45:34 +0800

    setting up redis for heroku

---
+++ config/initializers/redis.rb
@@ -0,0 +1,15 @@
+# config/initializers/redis.rb
+$redis = Redis.new
+
+url = ENV["REDISCLOUD_URL"]
+
+if url
+  Sidekiq.configure_server do |config|
+    config.redis = { url: url }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { url: url }
+  end
+  $redis = Redis.new(:url => url)
+end
