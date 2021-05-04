64c8a89cb392c9242de0829b182f014708f722a5
XiwayBanh <xiway.banh@gmail.com>
2021-05-04 22:39:04 +0800

    updated the edit tab function to enable edit for all tabs

--- app/javascript/scripts/edit_show_tab.js
+++ app/javascript/scripts/edit_show_tab.js
@@ -4,25 +4,23 @@ const editTabShow = () => {
       console.log(showTabEdit)
       showTabEdit.forEach((tab) => {
         tab.addEventListener('click', (e) => {
-          console.log(e.currentTarget.dataset.id)
-          document
-            .getElementById("tab-update").removeAttribute("class");
+          const currentTabId = e.currentTarget.dataset.id
+          document.getElementById(`tab_update_${currentTabId}`).removeAttribute("class");
         })
       });
-      // showTabEdit.addEventListener('click', ()=>{
-      //   console.log('showTabEdit is clicked')
-      //   document.getElementById("tab-update").removeAttribute("class");
-      // });
     }
 }
 
 const closeEditTabShow = () => {
   // console.log("closing share popup");
-  const closeEditTabShow = document.querySelector("#close-edit-tab-show");
-  if (closeEditTabShow) {
-    closeEditTabShow.addEventListener('click', ()=>{
-      document.getElementById("tab-update").setAttribute("class", "hidden");
-    });
+  const closeEditTabShow = document.querySelectorAll("#close-edit-tab-show");
+  if (closeEditTabShow.length) {
+    closeEditTabShow.forEach((close) => {
+      close.addEventListener('click', (e) => {
+        const closeCurrentTabId = e.currentTarget.dataset.id
+        document.getElementById(`tab_update_${closeCurrentTabId}`).setAttribute("class", "hidden");
+      });
+    })
   }
 }
 
