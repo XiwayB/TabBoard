11605f03cb051220ced4085cb7a0a652ad7522aa
XiwayBanh <xiway.banh@gmail.com>
2021-05-04 22:17:08 +0800

    update logo for tabs without icon to match current Ta.B logo

--- app/javascript/scripts/edit_show_tab.js
+++ app/javascript/scripts/edit_show_tab.js
@@ -1,18 +1,24 @@
 const editTabShow = () => {
-  // console.log("share popping up");
-    const showTabEdit = document.getElementById("tab-edit");
-    // console.log({showForm})
-    if (showTabEdit) {
-      showTabEdit.addEventListener('click', ()=>{
-        console.log('showTabEdit is clicked')
-        document.getElementById("tab-update").removeAttribute("class");
+    const showTabEdit = document.querySelectorAll("#tab-edit");
+    if (showTabEdit.length) {
+      console.log(showTabEdit)
+      showTabEdit.forEach((tab) => {
+        tab.addEventListener('click', (e) => {
+          console.log(e.currentTarget.dataset.id)
+          document
+            .getElementById("tab-update").removeAttribute("class");
+        })
       });
+      // showTabEdit.addEventListener('click', ()=>{
+      //   console.log('showTabEdit is clicked')
+      //   document.getElementById("tab-update").removeAttribute("class");
+      // });
     }
 }
 
 const closeEditTabShow = () => {
   // console.log("closing share popup");
-  const closeEditTabShow = document.getElementById("close-edit-tab-show");
+  const closeEditTabShow = document.querySelector("#close-edit-tab-show");
   if (closeEditTabShow) {
     closeEditTabShow.addEventListener('click', ()=>{
       document.getElementById("tab-update").setAttribute("class", "hidden");
